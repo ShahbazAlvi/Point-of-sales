81b80f2b76fe756387cdaf052b113aadb365c8aa
ShahbazAlvi <110836757+ShahbazAlvi@users.noreply.github.com>
2025-12-11 17:42:43 +0500

    g

---
+++ lib/compoents/AppTextfield.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:point_of_sale/compoents/AppColors.dart';
+
+class AppTextField extends StatefulWidget {
+  final TextEditingController controller;
+  final String label;
+  final TextInputType? keyboardType;
+  final IconData? icon;
+  final IconData? icons;
+  final VoidCallback? onToggleVisibility;
+  const AppTextField({super.key,
+    required this.controller,
+    required this.label,
+    this.keyboardType,
+    this.icon, this.icons,
+    this.onToggleVisibility, required String? Function(dynamic value) validator});
+
+  @override
+  State<AppTextField> createState() => _AppTextFieldState();
+}
+
+class _AppTextFieldState extends State<AppTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10)
+      ),
+      child: TextFormField(
+        controller: widget.controller,
+        keyboardType: widget.keyboardType,
+        decoration: InputDecoration(
+         // hintText: widget.label,
+          labelText: widget.label,
+          prefixIcon: widget.icon != null ? Icon(widget.icon, color: AppColors.secondary) : null,
+          suffixIcon: widget.icons!=null?IconButton(onPressed: widget.onToggleVisibility, icon: Icon(widget.icons)):null,
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(color: Colors.grey, width: 1.5),),
+
+        ),
+
+
+      ),
+    );
+  }
+}
